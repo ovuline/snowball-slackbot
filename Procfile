747959c20196b1353060e3cceb606c0a9eb9971f
Nicholas Servidio <servidionicholas@gmail.com>
2018-02-10 11:04:52 -0500

    Added express. Removed some easter eggs.

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: node lib/index.js
+web: node bin/snowball-init.js
--- bin/snowball-init.js
+++ bin/snowball-init.js
@@ -1,4 +1,8 @@
 #!/usr/bin/env node
+const express = require('express')
+const PORT = process.env.PORT || 5000
+
+express().listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 const snowball = require('../lib/index.js');
 

6ca9a9309346cee3dc3a698cf4add26908383c9c
Nicholas Servidio <servidionicholas@gmail.com>
2018-02-10 22:14:15 -0500

    removed call to express

--- bin/snowball-init.js
+++ bin/snowball-init.js
@@ -1,8 +1,4 @@
 #!/usr/bin/env node
-const express = require('express');
-const PORT = process.env.PORT || 5000;
-
-express().listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 const snowball = require('../lib/index.js');
 
